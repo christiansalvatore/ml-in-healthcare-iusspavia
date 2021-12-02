e4f4205a71a5514666a6c35262a4c37f3103a610
christiansalvatore <christian.salvatore@gmail.com>
2021-12-02 10:18:11 +0100

    Updt Lesson #2

--- code/01_machine-learning/ml.m
+++ code/01_machine-learning/ml.m
@@ -30,27 +30,60 @@ NPos = N1;
 %
 
 % Principal Components Analysis
-[coeff,score,latent] = pca(X);
+[coeff,score,latent] = pca(data);
+% Rows of X correspond to observations and columns to variables.
+
+%
+%
+%
 
 % Fisher Discriminant Ratio
-...
+% Try to implement the formula from lesson #1
+
+%
+%
+%
     
 % Clustering k-means
-idx = kmeans(X,k);
+k = []; % How many clusters?
+idx = kmeans(data, k);
+
+%
+%
+%
 
 % K-nearest neighbors
-idx = knnsearch(X,Y);
+% KNNSEARCH(X,Y) finds the nearest neighbor in X for each point in Y
+idx = knnsearch(data(1:129,:), data(130,:));
+predicted_label = labels(idx);
+
+%
+%
+%
 
 % Decision tree
-Mdl = fitctree(X,Y);
-label = predict(Mdl,X);
+Mdl = fitctree(data(1:129,:), labels(1:129));
+label = predict(Mdl, data(130,:));
+
+%
+%
+%
 
 % Support vector machine
-Mdl = fitcsvm(X,target,'KernelFunction','rbf',...
-    'BoxConstraint',Inf);
-label = predict(Mdl,X);
+Mdl = fitcsvm(X, target, 'KernelFunction', 'rbf',...
+    'BoxConstraint', Inf);
+label = predict(Mdl, X);
+
+%
+%
+%
 
 % Ensemble learning
 % https://it.mathworks.com/help/stats/framework-for-ensemble-learning.html#bsw8akh
-Mdl = fitcensemble(Tbl,ResponseVarName);
-label = predict(Mdl,X);
+Mdl = fitcensemble(data(1:129,:), labels(1:129));
+label = predict(Mdl, data(130,:));
+
+%
+%
+%
+
